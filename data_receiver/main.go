0239a5b73f4f3d7631c3784650e20bd02850f408
ryanzola <ryanzola@me.com>
2024-04-23 20:08:40 -0400

    Add websocket receiver and obu data generator

---
+++ data_receiver/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/ryanzola/toll-calculator/types"
+)
+
+func main() {
+	recv := NewDataReceiver()
+	http.HandleFunc("/ws", recv.handleWS)
+	http.ListenAndServe(":30000", nil)
+}
+
+type DataReceiver struct {
+	msgch chan types.OBUData
+	conn  *websocket.Conn
+}
+
+func NewDataReceiver() *DataReceiver {
+	return &DataReceiver{
+		msgch: make(chan types.OBUData, 128),
+	}
+}
+
+func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
+	u := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	conn, err := u.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dr.conn = conn
+
+	go dr.wsReceiveLoop()
+}
+
+func (dr *DataReceiver) wsReceiveLoop() {
+	fmt.Println("New OBU client connected")
+	for {
+		var data types.OBUData
+		if err := dr.conn.ReadJSON(&data); err != nil {
+			log.Println("read error:", err)
+			continue
+		}
+
+		fmt.Printf("Received OBU data from [%d] :: <lat %.2f, long %.2f>\n", data.OBUID, data.Lat, data.Long)
+		dr.msgch <- data
+	}
+}
